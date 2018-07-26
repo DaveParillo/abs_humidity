aa7d172ed3ce8c0a4bf9a4d61cdd90d9b2ec164b
DaveParillo <DaveParillo@users.noreply.github.com>
2018-07-25 19:03:31 -0700

    added makefile for unit tests. updated json header tests for API change

--- tests/test-util.cpp
+++ tests/test-util.cpp
@@ -4,30 +4,43 @@
 
 SCENARIO( "Convert characters to hex" ) {
     GIVEN( "a valid char" ) {
-	WHEN( "false should be returned for int input" ) {
-	    THEN( "false should be returned for int input" ) {
-		CHECK( 1 == 1);
-		//REQUIRE(to_hex(1) == false);
-	    }
-	}
+        WHEN( "false should be returned for int input" ) {
+            THEN( "false should be returned for int input" ) {
+                CHECK( 1 == 1);
+                //REQUIRE(to_hex(1) == false);
+            }
+        }
     }
 }
 SCENARIO( "Compute the invariants of to_hex" ) {
 
     GIVEN( "an invalid char" ) {
-	THEN( "false should be returned for int input" ) {
-	    //REQUIRE(to_hex(1) == false);
-	}
+        THEN( "false should be returned for int input" ) {
+            //REQUIRE(to_hex(1) == false);
+        }
     }
 }
 
 SCENARIO ("Check if json_header works") {
-    WHEN ("json header is called") {
-        THEN("content type for json is always returned") {
-            std::string expected = "Content-type: application/json\n\n";
+    std::string expected = "HTTP/1.1 200 OK\nContent-type: application/json\n\n";
+    WHEN ("json header is called with no parameters") {
+        THEN("content type for json and 200 OK returned") {
             CHECK(expected == json_header());
         }
 
     }
+    WHEN ("json header is passed StatusCode::ok") {
+        THEN("content type for json and 200 OK returned") {
+            CHECK(expected == json_header(StatusCode::ok));
+        }
 
-          }
+    }
+    WHEN ("json header is passed StatusCode::bad") {
+        THEN("content type for json and 400 Bad Request returned") {
+            expected = "HTTP/1.1 400 Bad Request\nContent-type: application/json\n\n";
+            CHECK(expected == json_header(StatusCode::bad));
+        }
+
+    }
+
+}
