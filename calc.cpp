e00a5de6276b1e08a28d98c71a66f1404a47ce4a
DaveParillo <DaveParillo@users.noreply.github.com>
2018-07-31 10:01:29 -0700

    removed input echoing, added input validation

--- calc.cpp
+++ calc.cpp
@@ -22,27 +22,42 @@ json make_json_pair(const string& uom, const double& value) {
     return {{"uom", uom}, {"value", value}};
 }
 
-json make_json_pair(const string& param, const string& uom, const double& value) {
-    return {{param, { {"uom", uom}, {"value", value} } }};
+bool numeric(string value) {
+    auto ss = std::stringstream(value);
+    double tmp;
+    ss >> tmp;
+    return !(!ss || ss.rdbuf()->in_avail() > 0);
 }
 
 response_t isvalid (const std::map<std::string, std::string>& query_params) {
-    json j;
-    response_t r {true, j};
+    response_t r = {true, {"status","success"}};
+
     // validate required air temp input
     auto it = query_params.find("air_temp");
-    if (it != query_params.end()) {
-        input.air_temp = atof(it->second.c_str());
+    if (it != query_params.end() && !it->second.empty()) {
+        if(numeric(it->second)) {
+            input.air_temp = atof(it->second.c_str());
+        } else {
+            r.valid = false;
+            r.doc["status"] = "error";
+            r.doc["message"] = "Non-numeric value provided for air_temp.";
+            r.doc["expected"] = "a floating point value [-45C - 60C]";
+            r.doc["actual"] = it->second;
+        }
+    } else if (it->second.empty()) {
+        r.valid = false;
+        r.doc["status"] = "error";
+        r.doc["message"] = "No value provided for air_temp input parameter.";
+        r.doc["expected"] = "a floating point value [-45C - 60C]";
+        r.doc["actual"] = it->second;
     } else {
         r.valid = false;
         r.doc["status"] = "error";
-        r.doc["message"] = "Service Error: Required input parameter not specified.";
+        r.doc["message"] = "Required input parameter not specified.";
         r.doc["expected"] = "air_temp";
         r.doc["actual"] = nullptr;
         return r;
     }
-
-    j = make_json_pair ("C", input.air_temp); 
 
     // validate optional uom for air temp
     it = query_params.find("uom");
@@ -50,39 +65,36 @@ response_t isvalid (const std::map<std::string, std::string>& query_params) {
         string uom = it->second;
         uom = toupper(uom[0]);
         if (uom == "F") {
-            j = make_json_pair ("F", input.air_temp); 
+            input.air_temp = cvt_f_c(input.air_temp);
+        } else if (uom == "C") {
+            // no action required
         } else {
             r.valid = false;
             r.doc["status"] = "error";
-            r.doc["message"] = "Service Error: Unknown unit of measure provided.";
+            r.doc["message"] = "Unknown unit of measure provided.";
             r.doc["expected"] = "One of 'uom=C' or 'uom=F'.";
             r.doc["actual"] = it->second;
             return r;
         }
-    }
-    if (!r.echo_input) {
-        //json in = {{"in", j}};
-        //r.doc = in;
-        r.doc["in"]["air_temp"] = j;
     }
     return r;
 }
 
 response_t calculate (const response_t& response) {
     auto r = response;
-    r.doc["status"] = "success";
 
     if (input.air_temp < -45 || input.air_temp > 60) {
         r.doc["status"] = "warn";
-        r.doc["message"] = 
+        r.doc["message"] =
               "The valid input limits for air temperature are between -45C and 60C";
     }
-    r.doc["out"]["vapor_pressure"] = make_json_pair("hPa", vapor_pres_wmo (input.air_temp));
+    r.doc["data"]["vapor_pressure"] = make_json_pair("hPa", vapor_pres_wmo (input.air_temp));
 
     auto ah = abs_humidity (input.air_temp);
-    r.doc["out"]["absolute_humidity"] = make_json_pair("kg/m**3", ah);
-//    r.doc["out"]["absolute_humidity"]["g/m**3"] = ah/1000;
+    r.doc["data"]["absolute_humidity"] = make_json_pair("kg/m**3", ah);
+    //r.doc["data"]["absolute_humidity"] = make_json_pair("g/m**3", ah/1000.0);
 
     return r;
 }
+
 
